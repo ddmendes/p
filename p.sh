af41d39715f9da17507911723c90d7e4f1e4a3d7
Davi Diorio Mendes <ddioriomendes@gmail.com>
2022-02-17 11:52:10 -0300

    Decouple get path routine

--- p.sh
+++ p.sh
@@ -3,13 +3,13 @@ P_PROPERTIES_FILE="${P_PROPERTIES_FILE:=$HOME/.pdata.properties}"
 
 p() {
     case $1 in
-    add) __add $2 $3 ;;
-    list) __list $2 ;;
+    set) __p_set $2 $3 ;;
+    list) __p_list $2 ;;
     *)
         if [ -z $1 ] ; then
             __p_help
         else 
-            __cd $1
+            __p_cd $1
         fi
     ;;
     esac
@@ -19,36 +19,44 @@ __p_help() {
     echo "P utility help"
 }
 
-__add() {
+__p_set() {
     touch $P_PROPERTIES_FILE
     echo $1=$2 >> $P_PROPERTIES_FILE
 }
 
-__list() {
+__p_list() {
     if [ -z $1 ] ; then
         cat `echo $P_PROPERTIES_FILE`
     else
-        _p_singleAlias=$(grep ^$1 $P_PROPERTIES_FILE)
+        local _p_singleAlias=$(grep ^$1 $P_PROPERTIES_FILE)
         if [ -z $_p_singleAlias ] ; then
             echo "Alias \"$1\" not found"
         else
             echo $_p_singleAlias
         fi
-        unset _p_singleAlias
     fi
 }
 
-__cd() {
+__p_cd() {
+    cd `__p_getPath $1`
+}
+
+__p_getPath() {
     if [ -z $1 ] ; then
-        echo "Missing alias name"
+        __p_err "Missing alias key"
         return 1
     fi
 
-    _p_path=$(grep ^$1 $P_PROPERTIES_FILE | cut -d"=" -f2)
+    local _p_path=$(grep ^$1 $P_PROPERTIES_FILE | cut -d"=" -f2)
     if [ -z $_p_path ] ; then
-        echo "No definition for alias \"$1\""
+        __p_err "No definition for alias \"$1\""
     fi
 
-    cd $_p_path
-    unset _p_path
+    echo $_p_path
+}
+
+__p_err() {
+    if [ -n $1 ] ; then
+        echo "p: $1" > /dev/stderr
+    fi
 }
