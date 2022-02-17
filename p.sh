9bdb5500e8b3b7d0072e535931e0d760f0d8611a
Davi Diorio Mendes <ddioriomendes@gmail.com>
2022-02-17 09:32:28 -0300

    Minimal functionality

--- p.sh
+++ p.sh
@@ -5,8 +5,18 @@ p() {
     case $1 in
     add) __add $2 $3 ;;
     list) __list $2 ;;
-    *)  ;;
+    *)
+        if [ -z $1 ] ; then
+            __p_help
+        else 
+            __cd $1
+        fi
+    ;;
     esac
+}
+
+__p_help() {
+    echo "P utility help"
 }
 
 __add() {
@@ -18,18 +28,28 @@ __list() {
     if [ -z $1 ] ; then
         cat `echo $P_PROPERTIES_FILE`
     else
-        singleAlias=$(grep $1 $P_PROPERTIES_FILE)
-        if [ -z $singleAlias ] ; then
+        _p_singleAlias=$(grep $1 $P_PROPERTIES_FILE)
+        if [ -z $_p_singleAlias ] ; then
             echo "Alias \"$1\" not found"
         else
-            echo $singleAlias
+            echo $_p_singleAlias
         fi
-        unset singleAlias
+        unset _p_singleAlias
     fi
 }
 
 __cd() {
-    
+    if [ -z $1 ] ; then
+        echo "Missing alias name"
+    fi
+
+    _p_path=$(grep $1 $P_PROPERTIES_FILE | cut -d"=" -f2)
+    if [ -z $_p_path ] ; then
+        echo "No definition for alias \"$1\""
+    fi
+
+    cd $_p_path
+    unset _p_path
 }
 
 
