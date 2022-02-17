235587aa72034b454d7fa7e4a5220fa6c3a631a0
Davi Diorio Mendes <ddioriomendes@gmail.com>
2022-02-17 14:17:26 -0300

    Enhance set implementation

--- p.sh
+++ p.sh
@@ -20,25 +20,41 @@ __p_help() {
 }
 
 __p_set() {
-    touch $P_PROPERTIES_FILE
-    echo $1=$2 >> $P_PROPERTIES_FILE
+    if [ -z $1 ] || [ -z $2 ] ; then
+        __p_err "Missing operands [alias] [path]"
+        return 1
+    fi
+
+    local lookup=$(__p_getPath $1)
+    if [ -z $lookup ] ; then
+        __p_add $1 $2
+    else 
+        __p_update $1 $2
+    fi
 }
 
 __p_list() {
     if [ -z $1 ] ; then
         cat `echo $P_PROPERTIES_FILE`
     else
-        local _p_singleAlias=$(grep ^$1 $P_PROPERTIES_FILE)
-        if [ -z $_p_singleAlias ] ; then
-            echo "Alias \"$1\" not found"
+        local singleAlias=$(grep ^$1 $P_PROPERTIES_FILE)
+        if [ -z $singleAlias ] ; then
+            __p_err "Alias \"$1\" does not exist"
+            return 1
         else
-            echo $_p_singleAlias
+            echo $singleAlias
         fi
     fi
 }
 
 __p_cd() {
-    cd `__p_getPath $1`
+    local path `__p_getPath $1`
+    if [ -z $path] ; then
+        __p_err "Alias \"$1\" does not exist"
+        return 1
+    fi
+
+    cd $1
 }
 
 __p_getPath() {
@@ -48,11 +64,18 @@ __p_getPath() {
     fi
 
     local _p_path=$(grep ^$1 $P_PROPERTIES_FILE | cut -d"=" -f2)
-    if [ -z $_p_path ] ; then
-        __p_err "No definition for alias \"$1\""
-    fi
-
     echo $_p_path
+}
+
+__p_add() {
+    touch $P_PROPERTIES_FILE
+    echo $1=$2 >> $P_PROPERTIES_FILE
+}
+
+# Code from here: https://gist.github.com/kongchen/6748525
+__p_update() {
+    awk -v pat="^$1=" -v value="$1=$2" '{ if ($0 ~ pat) print value; else print $0; }' $P_PROPERTIES_FILE > $P_PROPERTIES_FILE.tmp
+    mv $P_PROPERTIES_FILE{.tmp,}
 }
 
 __p_err() {
